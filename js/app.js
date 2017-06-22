dc8cc8fe636fa3c928dd24c0c09a322fe119bb51
fbk-rena <fbk.rena@gmail.com>
2017-06-22 13:19:26 -0500

    side menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
  var cargarPagina = function () {
-      $(".button-collapse").sideNav();
+     $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+    }
+  );
      $('.modal').modal();
      $('.carousel').carousel();
+     desplazamientoSuave();
+ };
+
+ var desplazamientoSuave = function () {
+
+     $('a[href^="#"]').click(function (event) {
+         var id = $(this).attr("href");
+         var offset = 20;
+         var target = $(id).offset().top-offset;
+
+         $('html, body').animate({
+             scrollTop: target
+         }, 750);
+         event.preventDefault();
+     });
  };
 
  $(document).ready(cargarPagina);
